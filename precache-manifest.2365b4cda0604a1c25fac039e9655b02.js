397642534eca106f17fcb36f938f417ba5f369c9
mcshawan <mcshawan@gmail.com>
2020-01-17 09:09:57 -0800

    Updates

---
+++ precache-manifest.2365b4cda0604a1c25fac039e9655b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d13757d1adf627622208aef91304f4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "23c8048fceb4bb11d83c",
+    "url": "/monsters-rolodex/static/css/main.e3c8c95a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23c8048fceb4bb11d83c",
+    "url": "/monsters-rolodex/static/js/main.9093235f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
